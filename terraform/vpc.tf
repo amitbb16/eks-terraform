9ae7e39e9465b72067475151f5c8772233ec4c0f
Amit B <122956425+amitbb16@users.noreply.github.com>
2024-01-13 02:15:54 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,8 +10,11 @@ module "vpc" {
   private_subnets = ["10.42.1.0/24", "10.42.2.0/24", "10.42.3.0/24"]
   public_subnets  = ["10.42.101.0/24", "10.42.102.0/24", "10.42.103.0/24"]
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+  enable_nat_gateway   = true
+  create_igw           = true
+  enable_vpn_gateway   = false
+  enable_dns_hostnames = true
+  single_nat_gateway   = true
 
   tags = {
     Terraform = "true"

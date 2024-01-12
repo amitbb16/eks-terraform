3a8fab3dfd919ed322b38ccebc4079c57a56602a
Amit B <122956425+amitbb16@users.noreply.github.com>
2024-01-13 00:10:57 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,6 +7,7 @@ terraform {
   }
 }
 provider "aws" {
-  // profile = "default"
-  region  = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }

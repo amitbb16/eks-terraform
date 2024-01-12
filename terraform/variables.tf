7c87f6cd4765ad0781ad997bed5b8b35392d85c9
Amit B <122956425+amitbb16@users.noreply.github.com>
2024-01-13 00:08:47 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,20 @@ variable "cluster_version" {
   type        = string
   default     = "1.28"
 }
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}

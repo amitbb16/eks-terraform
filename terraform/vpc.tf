104d2e5598c7c9f516a6de856a61ad9c757a84a5
Amit B <122956425+amitbb16@users.noreply.github.com>
2024-01-13 17:56:45 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,24 +1,43 @@
+locals {
+  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 3, k + 3)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 3, k)]
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.5"
 
   name = var.cluster_name
-  cidr = "10.42.0.0/16"
+  cidr = var.vpc_cidr
 
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.42.1.0/24", "10.42.2.0/24", "10.42.3.0/24"]
-  public_subnets  = ["10.42.101.0/24", "10.42.102.0/24", "10.42.103.0/24"]
+  azs             = local.azs
+  public_subnets        = local.public_subnets
+  private_subnets       = local.private_subnets
+  public_subnet_suffix  = "SubnetPublic"
+  private_subnet_suffix = "SubnetPrivate"
 
   enable_nat_gateway   = true
   create_igw           = true
-  enable_vpn_gateway   = false
   enable_dns_hostnames = true
   single_nat_gateway   = true
+
+  # Manage so we can name
+  manage_default_network_acl    = true
+  default_network_acl_tags      = { Name = "${var.cluster_name}-default" }
+  manage_default_route_table    = true
+  default_route_table_tags      = { Name = "${var.cluster_name}-default" }
+  manage_default_security_group = true
+  default_security_group_tags   = { Name = "${var.cluster_name}-default" }
 
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
-  
 }
